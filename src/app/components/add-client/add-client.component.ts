f1066eb406a02cf5b664dd6f8f67113fbcb41348
EslB <eslevano@hotmail.com>
2018-03-30 16:59:33 -0500

    Nuevo avance

--- src/app/components/add-client/add-client.component.ts
+++ src/app/components/add-client/add-client.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { Client } from '../../models/Client';
 
 @Component({
   selector: 'app-add-client',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-client.component.css']
 })
 export class AddClientComponent implements OnInit {
+  client: Client = {
+    firstName:'',
+    lastName:'',
+    email:'',
+    phone:'',
+    balance:0
+  }
+
+  disableBalanceOnAdd: boolean = true;
+
+  @ViewChild('clientForm') from:any;
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit({value, valid}: {value: Client, valid: boolean}){
+    if(this.disableBalanceOnAdd){
+      value.balance = 0;
+    }
+
   }
 
 }
